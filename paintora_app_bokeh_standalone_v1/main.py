d65e5fc7a30adb47729d10e87a39aa116aa2add3
mischiatim <31903224+mischiatim@users.noreply.github.com>
2021-11-17 23:15:10 -0500

    created custom classes module

--- paintora_app_bokeh_standalone_v1/main.py
+++ paintora_app_bokeh_standalone_v1/main.py
@@ -7,7 +7,7 @@ warnings.filterwarnings('ignore')
 from joblib import load
 
 import sklearn
-from sklearn.base import BaseEstimator, TransformerMixin
+#from sklearn.base import BaseEstimator, TransformerMixin
 
 from bokeh.layouts import column, row
 from bokeh.plotting import figure, curdoc #, show 
@@ -39,47 +39,49 @@ from bokeh.server.server import Server
 #
 # app = Flask(__name__)
 
+from custom_classes import ModelTransformer, DictEncoder, TagsEncoder
 
-class ModelTransformer(BaseEstimator, TransformerMixin):
+
+# class ModelTransformer(BaseEstimator, TransformerMixin):
     
-    def __init__(self, model):
-        self.predictor = model
+#     def __init__(self, model):
+#         self.predictor = model
             
-    def fit(self, X, y):
-        # Fit the stored predictor.
-        self.predictor.fit(X, y)
-        return self
+#     def fit(self, X, y):
+#         # Fit the stored predictor.
+#         self.predictor.fit(X, y)
+#         return self
     
-    def transform(self, X):
-        # Use predict on the stored predictor as a "transformation".
-        # reshape(-1,1) is required to return a 2-D array which is expected by scikit-learn.
-        return np.array(self.predictor.predict(X)).reshape(-1,1)
-    
-
-def stringlist_to_dict(stringlist):
-        dict = {}
-        for string in stringlist:
-            dict[string]=1
-        return dict
-    
-class DictEncoder(BaseEstimator, TransformerMixin):
-
-    def fit(self, X, y=None):
-        return self
-    
-    def transform(self, X):
-        # X will be a pandas series of strings representing lists of strings. Return a pandas series of dictionaries
-        return X.apply(eval).apply(stringlist_to_dict)
+#     def transform(self, X):
+#         # Use predict on the stored predictor as a "transformation".
+#         # reshape(-1,1) is required to return a 2-D array which is expected by scikit-learn.
+#         return np.array(self.predictor.predict(X)).reshape(-1,1)
     
 
-class TagsEncoder(BaseEstimator, TransformerMixin):
+# def stringlist_to_dict(stringlist):
+#         dict = {}
+#         for string in stringlist:
+#             dict[string]=1
+#         return dict
     
-    def fit(self, X, y=None):
-        return self
+# class DictEncoder(BaseEstimator, TransformerMixin):
+
+#     def fit(self, X, y=None):
+#         return self
     
-    def transform(self, X):
-        # X will be a pandas series of strings representing lists of strings. Return a pandas series of single strings containing all individual strings joined by a space
-        return X.apply(eval).apply(lambda stringlist:' '.join(stringlist))
+#     def transform(self, X):
+#         # X will be a pandas series of strings representing lists of strings. Return a pandas series of dictionaries
+#         return X.apply(eval).apply(stringlist_to_dict)
+    
+
+# class TagsEncoder(BaseEstimator, TransformerMixin):
+    
+#     def fit(self, X, y=None):
+#         return self
+    
+#     def transform(self, X):
+#         # X will be a pandas series of strings representing lists of strings. Return a pandas series of single strings containing all individual strings joined by a space
+#         return X.apply(eval).apply(lambda stringlist:' '.join(stringlist))
 
     
 # # For Flask/gunicorn deployment:
